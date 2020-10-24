dc7d099b81c6be70a0b077ae759d6cf2bf1efe80
Piotr Przybylak <piotr.przybylak@gmail.com>
2020-10-24 12:09:56 +0200

    Fix imports

--- src/main/java/coop/web/UsersController.java
+++ src/main/java/coop/web/UsersController.java
@@ -4,10 +4,8 @@ import coop.db.UserDAO;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @Controller
 public class UsersController {
