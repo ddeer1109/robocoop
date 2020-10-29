1f2080c6e93cf20dbf554e317c551214f55e9725
Piotr Przybylak <piotr.przybylak@gmail.com>
2020-10-29 21:04:54 +0100

    Fix clock timezone to Europe/Warsaw

--- src/main/java/coop/model/Clock.java
+++ src/main/java/coop/model/Clock.java
@@ -3,10 +3,11 @@ package coop.model;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 
 @Component
 public class Clock {
     public LocalDateTime now() {
-        return LocalDateTime.now();
+        return LocalDateTime.now(ZoneId.of("Europe/Warsaw"));
     }
 }
