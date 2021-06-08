02c970d188a1198ff50fa1fc05b53ba2e1757d74
Szymon Maj <jasm0012@interia.pl>
2021-06-08 15:05:32 +0200

    test cases for cart

--- src/test/java/org/masteukodeu/robocoop/model/CartTest.java
+++ src/test/java/org/masteukodeu/robocoop/model/CartTest.java
@@ -54,4 +54,69 @@ class CartTest {
 
         assertThat(result).isEqualByComparingTo(new BigDecimal(1650));
     }
+
+    @Test
+    public void testGetTotalZeroWithCommunityFund() {
+
+        ArrayList<Cart.Item> items = new ArrayList<>();
+        items.add(new Cart.Item("", new Product(null, null, new BigDecimal(100), null, null, 0), new BigDecimal(0)));
+        items.add(new Cart.Item("", new Product(null, null, new BigDecimal(200), null, null, 0), new BigDecimal(0)));
+        Cart cart = new Cart(items);
+
+        BigDecimal result = cart.getTotalWithCommunityFund();
+
+        assertThat(result).isEqualByComparingTo(new BigDecimal(0));
+    }
+
+    @Test
+    public void testGetTotalWithDecimal() {
+
+        ArrayList<Cart.Item> items = new ArrayList<>();
+        items.add(new Cart.Item("", new Product(null, null, new BigDecimal(100), null, null, 0), new BigDecimal(0.5)));
+        items.add(new Cart.Item("", new Product(null, null, new BigDecimal(200), null, null, 0), new BigDecimal(0.5)));
+        Cart cart = new Cart(items);
+
+        BigDecimal result = cart.getTotal();
+
+        assertThat(result).isEqualByComparingTo(new BigDecimal(150));
+    }
+
+    @Test
+    public void testGetTotalWithDecimalAndTomato() {
+
+        ArrayList<Cart.Item> items = new ArrayList<>();
+        items.add(new Cart.Item("", new Product(null, "Pomidory", new BigDecimal(100), null, null, 0), new BigDecimal(0.5)));
+        items.add(new Cart.Item("", new Product(null, null, new BigDecimal(200), null, null, 0), new BigDecimal(0.5)));
+        Cart cart = new Cart(items);
+
+        BigDecimal result = cart.getTotal();
+
+        assertThat(result).isEqualByComparingTo(new BigDecimal(150));
+    }
+
+    @Test
+    public void testGetTotalWithDecimalAndTomatoWithUnit() {
+
+        ArrayList<Cart.Item> items = new ArrayList<>();
+        items.add(new Cart.Item("", new Product(null, "Pomidory", new BigDecimal(100), "kg", null, 0), new BigDecimal(1)));
+        items.add(new Cart.Item("", new Product(null, null, new BigDecimal(200), null, null, 0), new BigDecimal(1)));
+        Cart cart = new Cart(items);
+
+        BigDecimal result = cart.getTotal();
+
+        assertThat(result).isEqualByComparingTo(new BigDecimal(300));
+    }
+
+    @Test
+    public void testGetTotalWithDecimalAndID() {
+
+        ArrayList<Cart.Item> items = new ArrayList<>();
+        items.add(new Cart.Item("", new Product("44", null, new BigDecimal(100), null, null, 0), new BigDecimal(1)));
+        items.add(new Cart.Item("", new Product("56", null, new BigDecimal(200), null, null, 0), new BigDecimal(1)));
+        Cart cart = new Cart(items);
+
+        BigDecimal result = cart.getTotal();
+
+        assertThat(result).isEqualByComparingTo(new BigDecimal(300));
+    }
 }
